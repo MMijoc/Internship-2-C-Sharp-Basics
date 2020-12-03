877088fa93a2382f329dfc8c89d9f0f12765eb79
MMijoc <markomijoc18@gmail.com>
2020-12-03 19:58:43 +0100

    Fixed minor bugs and refactored code

--- PlaylistMaker/PlaylistMaker/Program.cs
+++ PlaylistMaker/PlaylistMaker/Program.cs
@@ -1,12 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 
-enum error
+enum ResultType
 {
-	SUCCESS = 0,
-	FAILURE = (-1)
+	Success,
+	Failure = -1
 }
-
 
 namespace PlaylistMaker
 {
@@ -14,10 +13,11 @@ namespace PlaylistMaker
 	{
 		static int Main(string[] args)
 		{
+
 			return SelectMenu();
 		}
 
-		static int PrintMenu()
+		static void PrintMenu()
 		{
 			Console.WriteLine("Izbornik:");
 			Console.WriteLine("1 - Ispis cijele liste:");
@@ -33,106 +33,107 @@ namespace PlaylistMaker
 			Console.WriteLine("0 - izlaz");
 			Console.Write("Izbor: ");
 
-			return (int)error.SUCCESS;
 		}
 
 		static int SelectMenu()
 		{
-			var input = "";
-			var select = 0;
-			var isNumber = true;
-			var songID = 0;
-			var songName = "";
 			Dictionary<int, string> playListDictionary = new Dictionary<int, string>();
 
-			//uncomment for quick testing samples
-			//char c;
-			//string s;
-			//for (int i = 1; i < 10; i++) {
-			//	c = (char)('A' + i - 1);
-			//	s = c.ToString();
-			//	playListDictionary.Add(i, s);
-			//}
-
+			//testing samples
+			char c;
+			string s;
+			for (int i = 1; i < 10; i++) {
+				c = (char)('A' + i - 1);
+				s = c.ToString();
+				playListDictionary.Add(i, s);
+			}
 
 			while (true)
 			{
-				input = "";
-				select = 0;
-				isNumber = false;
-				songID = 0;
-				songName = "";
+				var input = "";
+				var songID = 0;
+				var songName = "";
 
 				PrintMenu();
-				isNumber = int.TryParse(input = Console.ReadLine(), out select);
+				var isNumber = int.TryParse(input = Console.ReadLine(), out var select);
 
-				if (isNumber != true)
-				{
+				if (isNumber != true) {
 					Console.WriteLine("Nepoznata naredba \"{0}\"\nPritnisni bilo koju tipku za nastavak . . .", input);
 					Console.ReadLine();
 					Console.Clear();
 					continue;
 				}
 
-				if (select == 1) {
-					PrintList(playListDictionary);
+				switch (select)
+				{
+					case 1:
+						PrintList(playListDictionary);
+						break;
+					case 2:
+						songID = InputNumber("Unesite redni broj pjesme: ");
+						PrintSong(playListDictionary, songID);
+						break;
+					case 3:
+						Console.Write("Unseite ime pjesme: ");
+						songName = Console.ReadLine();
+						songName = songName.Trim();
+						PrintSong(playListDictionary, songName);
+						break;
+					case 4:
+						while (string.IsNullOrEmpty(songName)) {
+							Console.Write("Unseite ime nove pjesme: ");
+							songName = Console.ReadLine();
+							songName = songName.Trim();
+						}
 
-				} else if (select == 2) {
-					songID = InputNumber("Unesite redni broj pjesme: ");
-					PrintSong(playListDictionary, songID);
+						if (ConfirmAction()) {
+							if (AddSong(playListDictionary, songName) == (int)ResultType.Success)
+								Console.WriteLine("Pjesma je dodana na kraj playliste!");
 
-				} else if (select == 3) {
-					Console.Write("Unseite ime pjesme: ");
-					songName = Console.ReadLine();
-					PrintSong(playListDictionary, songName);
+						} else {
+							Console.WriteLine("Promjene odbačene, pjesma nije dodana!");
 
-				} else if (select == 4) {
-					Console.Write("Unseite ime nove pjesme: ");
-					songName = Console.ReadLine();
+						}
+						break;
+					case 5:
+						songID = InputNumber("Unesite redni broj pjesme koju želite izbrisati: ");
+						DeleteSong(playListDictionary, songID);
+						break;
+					case 6:
+						Console.Write("Unseite ime pjesme koju zelite izbrisati: ");
+						songName = Console.ReadLine();
+						DeleteSong(playListDictionary, songName);
+						break;
+					case 7:
+						Console.WriteLine("Jeste li sigurni da stvarno zelite izbrisati cijelu listu?");
+						if (ConfirmAction())
+						{
+							DeletePlaylist(playListDictionary);
+							Console.WriteLine("Playlista je uspjesno izbrisana");
 
-					if (ConfirmAction() == true) {
-						if (AddSong(playListDictionary, songName) == (int)error.SUCCESS)
-							Console.WriteLine("Pjesma je dodana na kraj playliste!");
-					} else {
-						Console.WriteLine("Promjene odbačene, pjesma nije dodana!");
-					}
-
-				} else if (select == 5) {
-					songID = InputNumber("Unesite redni broj pjesme koju želite izbrisati: ");
-					DeleteSong(playListDictionary, songID);
-
-				} else if (select == 6) {
-					Console.Write("Unseite ime pjesme koju zelite izbrisati: ");
-					songName = Console.ReadLine();
-					DeleteSong(playListDictionary, songName);
-
-				} else if (select == 7) {
-					Console.WriteLine("Jeste li sigurni da stvarno zelite izbrisati cijelu listu?");
-					if (ConfirmAction() == true) {
-						DeletePlaylist(playListDictionary);
-						Console.WriteLine("Playlista je uspjesno izbrisana");
-
-					}
-
-				} else if (select == 8) {
-					songID = InputNumber("Unesite redni broj pjesme koju želite preimenovati: ");
-					RenameSong(playListDictionary, songID);
-
-				} else if (select == 9) {
-					ReorderSongs(playListDictionary);
-
-				} else if (select == 10) {
-					Console.WriteLine("Želite li nasumično izmješati playlisu?");
-					if (ConfirmAction() == true) {
-						ShufflePlaylist(playListDictionary);
-						Console.WriteLine("Playlista je uspješno izmješana");
-					}
-
-				} else if (select == 0) {
-					break;
-
-				} else {
-					Console.WriteLine("Nepoznata naredba \"{0}\"", input);
+						}
+						break;
+					case 8:
+						PrintList(playListDictionary);
+						songID = InputNumber("\nUnesite redni broj pjesme koju želite preimenovati: ");
+						RenameSong(playListDictionary, songID);
+						break;
+					case 9:
+						ReorderSongs(playListDictionary);
+						break;
+					case 10:
+						Console.WriteLine("Želite li nasumično izmješati playlisu?");
+						if (ConfirmAction())
+						{
+							ShufflePlaylist(playListDictionary);
+							Console.WriteLine("Playlista je uspješno izmješana");
+						}
+						break;
+					case 0:
+						return (int)ResultType.Success;
+					default:
+						Console.WriteLine("Nepoznata naredba \"{0}\"", input);
+						break;
 				}
 
 				Console.WriteLine("\nPritnisni bilo koju tipku za nastavak . . .");
@@ -140,204 +141,188 @@ namespace PlaylistMaker
 				Console.Clear();
 			}
 
-			return (int)error.SUCCESS;
+			
 		}
-
 		static int InputNumber(string message)
 		{
-			var input = "";
-			int number;
-			bool isNumber = false;
-
-			do
-			{
+			while (true) {
 				if (message != "")
 					Console.Write(message);
-				isNumber = int.TryParse(input = Console.ReadLine(), out number);
+				bool isNumber = int.TryParse(Console.ReadLine(), out int number);
 
-				if (isNumber == true)
+				if (isNumber)
 					return number;
 				else
 					Console.WriteLine("Nepravilan unos!");
-
-			} while (isNumber == false);
-
-			return int.MinValue;
+			} 
 		}
 		static int PrintList(Dictionary<int, string> playList)
 		{
-			int n = playList.Count;
-			if (n == 0) {
+			int count = playList.Count;
+			if (count == 0) {
 				Console.WriteLine("Playlista je prazna!");
-				return (int)error.FAILURE;
+				return (int)ResultType.Failure;
 			}
 
 			Console.WriteLine("\n{0, -8} {1, -32}", "ID", "Song name");
-			for (int i = 1; i <= n; i++)
+			for (int i = 1; i <= count; i++)
 				Console.WriteLine("{0, -8} {1, -32}", i, playList[i]);
 
-			return (int)error.SUCCESS;
+			return (int)ResultType.Success;
 		}
 
 		static int PrintSong(Dictionary<int, string> playList, int songID)
 		{
-			var exits = false;
-			foreach (KeyValuePair<int, string> song in playList) {
+			var songExists = false;
+			foreach (var song in playList) {
 				if (song.Key == songID) {
 					Console.WriteLine("{0, -8} {1, -32}", song.Key, song.Value);
-					exits = true;
+					songExists = true;
 					break;
 				}
 			}
 
-			if (exits != true) {
+			if (!songExists) {
 				Console.WriteLine("Ne postoji pjesma s rednim brojem \"{0}\"", songID);
-				return (int)error.FAILURE;
+				return (int)ResultType.Failure;
 			}
 
-			return (int)error.SUCCESS;
+			return (int)ResultType.Success;
 		}
 
 		static int PrintSong(Dictionary<int, string> playList, string songName) {
-			var exits = false;
-			foreach (KeyValuePair<int, string> song in playList)
-			{
-				if (song.Value == songName)
-				{
+			var songExists = false;
+
+			foreach (var song in playList) {
+				if (songName.Equals(song.Value, StringComparison.OrdinalIgnoreCase)) {
 					Console.WriteLine("{0, -8} {1, -32}", song.Key, song.Value);
-					exits = true;
+					songExists = true;
 					break;
 				}
 			}
 
-			if (exits != true) {
+			if (!songExists) {
 				Console.WriteLine("Ne postoji pjesma s imenom \"{0}\"", songName);
-				return (int)error.FAILURE;
+				return (int)ResultType.Failure;
 			}
 
-			return (int)error.SUCCESS;
-
+			return (int)ResultType.Success;
 		}
 
 		static int AddSong(Dictionary<int, string> playList, string songName)
 		{
-			var newSongID = playList.Count;
-			newSongID++;
+			var newSongId = playList.Count + 1;
 
-			foreach (KeyValuePair<int, string> song in playList)
+			foreach (var song in playList)
 				if (song.Value == songName) {
 					Console.WriteLine("Pjesma s imenom \"{0}\" već postoji!", songName);
-					return (int)error.FAILURE;
+					return (int)ResultType.Failure;
 				}
 
-			playList.Add(newSongID, songName);
+			playList.Add(newSongId, songName);
 
-			return (int)error.SUCCESS;
+			return (int)ResultType.Success;
 		}
 
 		static int DeleteSong(Dictionary<int, string> playList, int songID)
 		{
-			bool isDeleted = false;
 			string tmp;
-			int n = playList.Count;
+			int count = playList.Count;
 
-			if (playList.ContainsKey(songID) != true) {
-				Console.WriteLine("Ne psotoji pjesma s rednim brojem \"{0}\"", songID);
-				return (int)error.FAILURE;
+			if (!playList.ContainsKey(songID)) {
+				Console.WriteLine("Ne postoji pjesma s rednim brojem \"{0}\"", songID);
+				return (int)ResultType.Failure;
 			}
 
-			Console.Write("Jeste li sigurni da zelite obrisati:\n\t");
+			Console.Write("Jeste li sigurni da želite obrisati:\n\t");
 			PrintSong(playList, songID);
 
-			if (ConfirmAction() == false) return (int)error.SUCCESS;
+			if (!ConfirmAction()) return (int)ResultType.Success;
 
-			isDeleted = playList.Remove(songID);
-			if (isDeleted != true)
-			{
-				Console.WriteLine("Nije moguće obrisati pjesmu s rednim brojm \"{0}\"", songID);
-				return (int)error.FAILURE;
+			var isDeleted = playList.Remove(songID);
+			if (!isDeleted) {
+				Console.WriteLine("Nije moguće obrisati pjesmu s rednim brojem \"{0}\"", songID);
+				return (int)ResultType.Failure;
 			}
 
 			songID++;
-			for (int i = songID; i <= n; i++)
-			{
+			for (int i = songID; i <= count; i++) {
 				tmp = playList[i];
 				playList.Remove(i);
 				playList.Add(i - 1, tmp);
 			}
 
-
-			return (int)error.SUCCESS;
+			return (int)ResultType.Success;
 		}
 
 		static int DeleteSong(Dictionary<int, string> playList, string songName)
 		{
 			int key = 0;
 
-			foreach (KeyValuePair<int, string> song in playList)
-				if (song.Value == songName)
+			foreach (var song in playList)
+				if (songName.Equals(song.Value, StringComparison.OrdinalIgnoreCase))
 					key = song.Key;
 
 			if (key != 0) {
 				DeleteSong(playList, key);
-				return (int)error.SUCCESS;
+				return (int)ResultType.Success;
 			} else {
 				Console.WriteLine("Ne postoji pjesma s imenom {0}", songName);
-				return (int)error.FAILURE;
+				return (int)ResultType.Failure;
 			}
 		}
 
 		static bool ConfirmAction()
 		{
-			string input = "";
 			Console.WriteLine("Jeste li sigurni da zelite nastaviti?\n\tDA - da želim nastaviti\n\tNE - ne zelim nastaviti (unesi bilo koju tipku za prekid)");
-			input = Console.ReadLine();
+			var input = Console.ReadLine();
 
 			return input.Equals("da", StringComparison.OrdinalIgnoreCase);
 		}
 
 		static int DeletePlaylist(Dictionary<int, string> playList)
 		{
-			int n = playList.Count;
-			bool success = true;
+			int count = playList.Count;
+			bool wasDeleted = true;
 			int i = 1;
 
-			while (success == true && i <= n) {
-				success = playList.Remove(i);
+			
+			while (wasDeleted == true && i <= count) {
+				wasDeleted = playList.Remove(i);
 				i++;
 			}
 
-			if (success == true)
-				return (int)error.SUCCESS;
+			if (wasDeleted)
+				return (int)ResultType.Success;
 			else
-				return (int)error.FAILURE;
+				return (int)ResultType.Failure;
 		}
 
 		static int RenameSong(Dictionary<int, string> playList, int songID)
 		{
 			if (playList.ContainsKey(songID) != true) {
 				Console.WriteLine("Ne psotoji pjesma s rednim brojem \"{0}\"", songID);
-				return (int)error.FAILURE;
+				return (int)ResultType.Failure;
 			}
 
 			Console.Write("Kako zelite preimenovati pjesmu  \"{0}\": ", playList[songID]);
 			string input = Console.ReadLine();
 			Console.WriteLine("Premienuj \"{0}\" u \"{1}\"", playList[songID], input);
-			if (ConfirmAction() == true) {
+			if (ConfirmAction()) {
 				Console.WriteLine("Pjesma \"{0}\" je preimenovana u \"{1}\"", playList[songID], input);
 				playList[songID] = input;
 			}
 
-			return (int)error.SUCCESS;
+			return (int)ResultType.Success;
 		}
 
 		static int ReorderSongs(Dictionary<int, string> playList)
 		{
-			bool contains = false;
-			int n = playList.Count;
+			bool contains;
+			int conut = playList.Count;
 			int i;
-			int songID_1 = 0;
-			int songID_2 = 0;
+			int songID_1;
+			int songID_2;
 
 			Dictionary<int, string> tmpDict = new Dictionary<int, string>(playList);
 			PrintList(playList);
@@ -345,7 +330,7 @@ namespace PlaylistMaker
 			while (true) {
 				songID_1 = InputNumber("\nUnesite redni broj pjesme koju želite premjestit: ");
 				contains = playList.ContainsKey(songID_1);
-				if (songID_1 > 0 && songID_1 <= n)
+				if (contains)
 					break;
 				Console.WriteLine("Ne postoji pjesma s rednim brojem \"{0}\"", songID_1);
 			}
@@ -353,14 +338,14 @@ namespace PlaylistMaker
 			while (true) {
 				songID_2 = InputNumber("\nUnesite redni broj na koji zelite prmejstiti odabranu pjesmu: ");
 				contains = playList.ContainsKey(songID_2);
-				if (songID_2 > 0 && songID_2 <= n)
+				if (contains)
 					break;
 				Console.WriteLine("Ne postoji pjesma s rednim brojem \"{0}\"", songID_2);
 			}
 
 			Console.WriteLine("Premjsti pjesmu s rednim brojem {0}  \"{1}\" na mjesto {2}", songID_1, playList[songID_1], songID_2);
-			if (ConfirmAction() == false)
-				return (int)error.SUCCESS;
+			if (!ConfirmAction())
+				return (int)ResultType.Success;
 
 
 			if (songID_1 > songID_2) {
@@ -373,7 +358,7 @@ namespace PlaylistMaker
 				for (i+= 1; i <= songID_1; i++)
 					playList.Add(i, tmpDict[i-1]);
 
-				for (; i <= n; i++)
+				for (; i <= conut; i++)
 					playList.Add(i, tmpDict[i]);
 
 			} else if (songID_1 < songID_2) {
@@ -386,14 +371,11 @@ namespace PlaylistMaker
 
 				playList.Add(i, tmpDict[songID_1]);
 
-				for (i += 1; i <= n; i++)
+				for (i += 1; i <= conut; i++)
 					playList.Add(i, tmpDict[i]);
 			}
 
-			//PrintList(tmpDict);
-			//PrintList(playList);
-
-			return (int)error.SUCCESS;
+			return (int)ResultType.Success;
 		}
 
 		static int ShufflePlaylist(Dictionary<int, string> playList)
@@ -418,7 +400,7 @@ namespace PlaylistMaker
 			for (int i = 0; i < n; i++)
 				playList.Add(i + 1, tmpDict[arr[i]]);
 
-			return (int)error.SUCCESS;
+			return (int)ResultType.Success;
 		}
 	}
 }
